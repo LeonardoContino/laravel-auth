5057dea85090cb8c48b94bc68ed3f80950ee3622
LeonardoContino <leoconti2000@hotmail.it>
2023-03-07 15:36:14 +0100

    aggiunto 2 controller e cartellle in view

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guest\Homecontroller as GuestHomeController;
+use App\Http\Controllers\Admin\Homecontroller as AdminHomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,18 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [GuestHomecontroller::class, 'index']);
+
+Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(function () {
+
+ Route::get('/',[AdminHomeController::class, 'index'])->name('home');
+
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware('auth')->name('profile.')-prefix('/profile')->group(function () {
+    Route::get('/', [ProfileController::class, 'edit'])->name('edit');
+    Route::patch('/', [ProfileController::class, 'update'])->name('update');
+    Route::delete('/', [ProfileController::class, 'destroy'])->name('destroy');
 });
 
 require __DIR__.'/auth.php';
